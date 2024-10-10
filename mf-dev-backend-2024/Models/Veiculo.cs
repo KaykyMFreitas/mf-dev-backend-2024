d9697a768a84a4b93a65d4be72227bc942a93fa1
Kayky <kafreitas.2015@gmail.com>
2024-10-09 21:12:51 -0300

    Aplicação completa com todas as funcionalidade

--- mf-dev-backend-2024/Models/Veiculo.cs
+++ mf-dev-backend-2024/Models/Veiculo.cs
@@ -19,5 +19,7 @@ namespace mf_dev_backend_2024.Models
         [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
         [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; } 
     }
 }
--- mf-dev-backend-2024/Program.cs
+++ mf-dev-backend-2024/Program.cs
@@ -1,5 +1,7 @@
 using mf_dev_backend_2024.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,19 @@ builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.Configure<CookiePolicyOptions>(option =>
+{
+    option.CheckConsentNeeded = context => true;
+    option.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.AccessDeniedPath = "/Usuarios/AccessDenied/";
+        options.LoginPath = "/Usuarios/Login/";
+    });
 
 var app = builder.Build();
 
@@ -26,6 +41,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
